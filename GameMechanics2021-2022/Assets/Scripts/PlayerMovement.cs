4c817fae93852e6095bf976e9ee8a4d57da6f6f5
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-09-25 13:44:28 +0200

    Finally figured out how to use the new Unity Input Package

--- GameMechanics2021-2022/Assets/Scripts/PlayerMovement.cs
+++ GameMechanics2021-2022/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,43 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public InputActionAsset playerControls;
+
+    [SerializeField] private float m_JumpStrength;
     private Vector3 m_Direction;
+    private Transform m_Transform;
+
+    private InputAction m_Movement;
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        m_Transform = gameObject.transform;
+
+        var playerActionMap = playerControls.FindActionMap("Player");
+
+       m_Movement = playerActionMap.FindAction("Movement");
+       m_Movement.performed += OnMovement;
+       m_Movement.canceled += OnMovement;
+       m_Movement.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void OnMovement(InputValue inputValue)
+    private void FixedUpdate()
     {
-        Debug.Log(inputValue.ToString());
+        m_Transform.Translate(m_Direction);
+
+        m_Direction.y -= 9.81f;
+    }
+
+    private void OnMovement(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump!");
+        m_Direction.y += m_JumpStrength;
     }
 }
