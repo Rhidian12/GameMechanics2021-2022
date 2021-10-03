f14d2302d702be76729924226d4e0462d51427f4
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-10-03 13:21:02 +0200

    backup

--- GameMechanics2021-2022/Assets/Scripts/PlayerAim.cs
+++ GameMechanics2021-2022/Assets/Scripts/PlayerAim.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAim : MonoBehaviour
 {
     [SerializeField] private Camera m_Camera;
 
     private Vector2 m_MousePosition;
+    private Vector2 m_CurrentRotation;
 
     // Update is called once per frame
     void Update()
@@ -14,16 +16,24 @@ public class PlayerAim : MonoBehaviour
         HandleInput();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         // Calculate rotation based on mouse position
-        Vector3 newForward = m_Camera.ScreenToWorldPoint(new Vector3(m_MousePosition.x, m_MousePosition.y, m_Camera.nearClipPlane));
+        m_CurrentRotation.x += m_MousePosition.x;
+        m_CurrentRotation.y -= m_MousePosition.y;
 
-        m_Camera.transform.forward = newForward.normalized;
+        m_CurrentRotation.x = Mathf.Repeat(m_CurrentRotation.x, 360);
+        m_CurrentRotation.y = Mathf.Clamp(m_CurrentRotation.y, -80f, 80f);
+
+        m_Camera.transform.rotation = Quaternion.Euler(m_CurrentRotation.y, m_CurrentRotation.x, 0);
+
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void HandleInput()
     {
-        m_MousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        var currentInput = Mouse.current;
+        if (currentInput != null)
+            m_MousePosition = currentInput.position.ReadValue();
     }
 }
